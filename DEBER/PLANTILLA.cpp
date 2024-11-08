24d86b5efb913cca0c2b9bfcd39368dce37fa90a
Mario-Anrrango <mariosanti280704@gmail.com>
2024-11-08 08:24:56 -0500

    ESTRCUTURA DE DATOS

---
+++ DEBER/PLANTILLA.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+//**Declaración de la plantilla de la clase Calculo **
+template <typename T>
+
+class Calculo{
+    public:
+    Calculo(T a, T b) : a(a), b(b){
+    }
+
+    T sumar();
+
+    T restar();
+
+    T multiplicar();
+
+    private:
+        T a;
+        T b;
+};
