817552de0feab3a19feff9a62146a4ba46989fd1
Mario-Anrrango <mariosanti280704@gmail.com>
2024-11-10 14:27:04 -0500

    ESTRUCTURA DE DATOS

---
+++ Validaciones.h
@@ -0,0 +1,10 @@
+#ifndef __VALIDACIONES_H
+#define __VALIDACIONES_H
+#include <string>
+
+class Validaciones {
+public:
+    string capturarCadenaNumerica(char* msj, bool permitePunto = false);
+};
+
+#endif
